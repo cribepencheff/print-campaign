e510e8b67cb1641a9d3cf4b1b89a2b742339a677
Cribe Pencheff <cribe@self-made.se>
2026-05-28 16:50:55 +0200

    feature: Add studio structure, schema and fileds for file-upload

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,18 +1,22 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { settings } from "./singletons/settings";
 import { page } from "./documents/page";
+import { motiveSubmission } from "./documents/motiveSubmission";
 import { hero } from "./sections/hero";
 import { textSection } from "./sections/textSection";
+import { fileUpload } from "./sections/fileUpload";
 import { blockContent } from "./ui/blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     // Page types
     page,
+    motiveSubmission,
 
     // Sections - Reusable content blocks that can be added to pages
     hero,
     textSection,
+    fileUpload,
 
     // UI - Custom input components and portable text definitions
     blockContent,
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,17 +1,54 @@
 import type { StructureResolver } from "sanity/structure";
+import { apiVersion } from "./env";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title("Content")
-    .items([
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() !== "settings",
-      ),
+    .title("Innehåll")
 
+    .items([
+      S.documentTypeListItem("page").title("Sidor"),
+
+      S.divider(),
+      S.divider(),
+
+      S.divider().title("Motiv"),
+
+      S.listItem()
+        .title("Inkommen")
+        .child(
+          S.documentTypeList("motiv")
+            .title("Inkommen")
+            .filter('_type == "motiv" && status == "inkommen"')
+            .apiVersion(apiVersion),
+        ),
+
+      S.listItem()
+        .title("Godkänd")
+        .schemaType("motiv")
+        .child(
+          S.documentTypeList("motiv")
+            .title("Godkänd")
+            .schemaType("motiv")
+            .filter('_type == "motiv" && status == "godkänd"')
+            .apiVersion(apiVersion),
+        ),
+
+      S.listItem()
+        .title("Nekad")
+        .schemaType("motiv")
+        .child(
+          S.documentTypeList("motiv")
+            .title("Nekad")
+            .schemaType("motiv")
+            .filter('_type == "motiv" && status == "nekad"')
+            .apiVersion(apiVersion),
+        ),
+
+      S.divider(),
       S.divider(),
 
       S.listItem()
-        .title("Settings")
+        .title("Webbplatsinställningar")
         .child(S.document().schemaType("settings").documentId("settings")),
     ]);
